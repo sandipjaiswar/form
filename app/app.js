0ce26973902c9807ce5d7c6e223225ad717f1643
sandipjaiswar <sandipjaiswar@gmail.com>
2018-11-21 01:28:42 +0530

    add folder structur and home page

--- app/app.js
+++ app/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const routes = require('./routes/index');
 const app = express();
+
 app.set('view engine', 'pug');
 app.set('views', './app/views');
+
+app.use(express.static('./app/public'))
+
 app.use('/', routes);
 
 module.exports = app;
